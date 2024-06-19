b3885fac838e54a295c9dbc56a5d04fb27ace4ec
Mehriban Khalilova <tu833KMN0@code.edu.az>
2024-06-19 21:48:49 +0400

    deploy again

--- api/app.js
+++ api/app.js
@@ -23,7 +23,7 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://booking-website-wvdj.onrender.com'],
   credentials:true
 }));
 app.use(cookieParser())
